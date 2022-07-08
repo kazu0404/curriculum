b5fc10933015a8332b6598a2b471e38e241906a3
KAZU <32030840+kazu0404@users.noreply.github.com>
2022-07-08 16:51:06 +0700

    updated
    updated

--- PHPjob/4.1/test2/dbInfo.php
+++ PHPjob/4.1/test2/dbInfo.php
@@ -43,7 +43,7 @@ if (isset($_POST["signUp"])) {
             $stmt->execute();  // 
             $userid = $dbh->lastinsertid();  // 登録した(DB側でauto_incrementした)IDを$useridに入れる
             $signUpMessage = '登録が完了しました。あなたの登録IDは ' . $userid . ' です。パスワードは ' . $password . ' です。';  // ログイン時に使用するIDとパスワード
-            echo $signUpMessage;
+            //echo $signUpMessage;
         } catch (PDOException $e) {
             $errorMessage = 'データベースエラー';
             // $e->getMessage() でエラー内容を参照可能（デバック時のみ表示）
@@ -56,3 +56,36 @@ if (isset($_POST["signUp"])) {
 }
 
 ?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>新規登録</title>
+    </head>
+    
+    <body>
+        <h1>新規登録画面</h1>
+        <form id="loginForm" name="loginForm" action="dbinfo.php" method="POST">
+            <fieldset>
+                <legend>新規登録フォーム</legend>
+                <div><font color="#ff0000"><?php echo htmlspecialchars($errorMessage, ENT_QUOTES); ?></font></div>
+                <div><font color="#0000ff"><?php echo htmlspecialchars($signUpMessage, ENT_QUOTES); ?></font></div>
+                <label for="name">ユーザー名</label>
+                <input type="text" id="name" name="name" value="" placeholder="ユーザー名を入力">
+                <br>
+                <label for="password">パスワード</label>
+                <input type="password" id="password" name="password" value="" placeholder="パスワードを入力">
+                <br>
+                <label for="password2">パスワード(確認用)</label>
+                <input type="password" id="password2" name="password2" value="" placeholder="再度パスワードを入力">
+                <br>
+                <input type="submit" id="signUp" name="signUp" value="登録">
+            </fieldset>
+        </form>
+    </body>
+</html>
+
+
+
+
